4f7a13902038a2acffebe2effb9672e24c6cb973
daniel-d7a <eyad.taym.saba@gmail.com>
2026-02-11 14:23:30 +0200

    changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 4000,
+    strictPort: true,
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
